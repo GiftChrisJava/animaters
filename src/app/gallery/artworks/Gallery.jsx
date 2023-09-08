7a6090585535f82b171e838ec493965af34941e4
GiftChrisJava <chrisjava77@gmail.com>
2023-09-08 06:35:30 -0700

    error resolved i think

--- src/app/gallery/artworks/Gallery.jsx
+++ src/app/gallery/artworks/Gallery.jsx
@@ -77,7 +77,7 @@ export const Gallery = () => {
       <div className={styles.images}>
         {images.map((image) => (
           <div key={image.id} className={styles.image_card}>
-            <Link href={`/gallery/artworks/${image.id}`}>
+            <Link href={`/gallery/artwork/${image.id}`}>
               <Image src={image.url} alt={image.alt} className={styles.img} />
               <div className={styles.image_info}>
                 <p className={styles.image_desc}>By {image.artist}</p>
