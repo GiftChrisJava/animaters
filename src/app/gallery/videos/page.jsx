471edb409a0a45c9be71f8df8cc9d5d361e6e9d7
GiftChrisJava <chrisjava77@gmail.com>
2023-09-08 18:33:45 -0700

    imported

--- src/app/gallery/videos/page.jsx
+++ src/app/gallery/videos/page.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "./vid.module.css";
+import Thumbnails from "./Thumbnails";
 
 export default function Videos() {
   return (
     <div className={styles.container}>
       <h1 className={styles.h1}>Video Collection</h1>
+      <Thumbnails />
     </div>
   );
 }
