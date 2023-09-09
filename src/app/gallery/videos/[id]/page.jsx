7e99033a53a7a933dbcc05ef0e1f33f5474db2d7
GiftChrisJava <chrisjava77@gmail.com>
2023-09-08 21:38:18 -0700

    started creating video page view

---
+++ src/app/gallery/videos/[id]/page.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import Image from "next/image";
+
+import styles from "./page.module.css";
+
+import { AiFillHeart } from "react-icons/ai";
+
+import thumbnail1 from "../../../../../public/thumbnail1.png";
+import thumbnail2 from "../../../../../public/thumbnail2.png";
+import thumbnail3 from "../../../../../public/thumbnail3.png";
+import thumbnail4 from "../../../../../public/thumbnail4.png";
+import thumbnail5 from "../../../../../public/thumbnail5.png";
+import thumbnail6 from "../../../../../public/thumbnail6.png";
+import thumbnail7 from "../../../../../public/thumbnail7.png";
+
+const videos = [
+  {
+    id: 1,
+    artist: "Haywood Liuma Java",
+    alt: "artwork",
+    url: "",
+    desc: "Short film about our works and various projects",
+    thumbnailURL: thumbnail1,
+  },
+
+  {
+    id: 2,
+    artist: "James Kaonga",
+    alt: "artwork",
+    url: "",
+    desc: "Short film about our works and various projects",
+    thumbnailURL: thumbnail2,
+  },
+  {
+    id: 5,
+    artist: "Mayamiko Juma",
+    alt: "artwork",
+    url: "",
+    desc: "Short film about our works and various projects",
+    thumbnailURL: thumbnail5,
+  },
+
+  {
+    id: 3,
+    artist: "Caleb Clouds",
+    alt: "artwork",
+    url: "",
+    desc: "Short film about our works and various projects",
+    thumbnailURL: thumbnail3,
+  },
+
+  {
+    id: 4,
+    artist: "Sizothini",
+    alt: "artwork",
+    url: "",
+    desc: "Short film about our works and various projects",
+    thumbnailURL: thumbnail4,
+  },
+
+  {
+    id: 7,
+    artist: "James Kaonga",
+    alt: "artwork",
+    url: "",
+    desc: "Short film about our works and various projects",
+    thumbnailURL: thumbnail7,
+  },
+
+  {
+    id: 6,
+    artist: "James Kaonga",
+    alt: "artwork",
+    url: "",
+    desc: "Short film about our works and various projects",
+    thumbnailURL: thumbnail6,
+  },
+];
+
+export const page = ({ params }) => {
+  const video = videos.find((vid) => vid.id == params.id);
+
+  return <div></div>;
+};
