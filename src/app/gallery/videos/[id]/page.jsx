70c1e42a68daac8cf5ff1bf4a2ddcb13747e3bd4
GiftChrisJava <chrisjava77@gmail.com>
2023-09-10 07:03:09 -0700

    oky

--- src/app/gallery/videos/[id]/page.jsx
+++ src/app/gallery/videos/[id]/page.jsx
@@ -93,6 +93,7 @@ export default function Page({ params }) {
 		className={styles.video}
                 url={video.url}
                 controls
+               width="100%"
                 
               />
 	 </div>
